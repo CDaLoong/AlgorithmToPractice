3c2f8cc5a8efdc2f32424a877eaf58a5d45ae022
陈雷龙 <chenleilong>
2022-03-16 16:49:37 +0800

    feat: Day7-2

---
+++ JavaScript语言/刷力扣/两数之和.js
@@ -0,0 +1,41 @@
+// 放在开始
+// 1. 严谨性判断，不允许任何报错
+// 2. 任何递归程序，先写出口
+// 3. 任何一种算法，都没有优劣之分，只有是否适合的场景
+
+// 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+// 你可以按任意顺序返回答案。
+
+// 示例 1：
+// 输入：nums = [2,7,11,15], target = 9
+// 输出：[0,1]
+// 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1]
+
+// 示例 2：
+// 输入：nums = [3,2,4], target = 6
+// 输出：[1,2]
+
+// 示例 3：
+// 输入：nums = [3,3], target = 6
+// 输出：[0,1]
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    if(!Array.isArray(nums) || nums.length === 0 || isNaN(target)) return [];
+    for(let i = 0; i < nums.length / 2; i++) {
+        for(let j = nums.length - 1; j > nums.length / 2 - 1; j-- ) {
+            if (getSum(nums[i] + nums[j]) === target) return [i, j]
+        }
+    }
+};
+
+const getSum = function(a, b) {
+    return a + b;
+}
+
+console.log(twoSum([2,7,11,15], 9))

6ee687a1c379e5f8eced3aa8f8a358fd50ca1041
DaLoong <CDaLoong@gmail.com>
2023-02-14 10:32:16 +0800

    feat: 力扣第7题

--- JavaScript语言/src/刷力扣/7. 整数反转.js
+++ JavaScript语言/src/刷力扣/7. 整数反转.js
@@ -1,7 +1,7 @@
 /**
  * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
 
-如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+如果反转后整数超过 32 位的有符号整数的范围 [−2^31,  2^31 − 1] ，就返回 0。
 
 假设环境不允许存储 64 位整数（有符号或无符号）。
  
@@ -26,5 +26,18 @@
 
 提示：
 
--231 <= x <= 231 - 1
+-2^31 <= x <= 2^31 - 1
  */
+
+const x = -123;
+
+const func = (x) => {
+  if (x < -Math.pow(2, 31) || x > Math.pow(2, 31) - 1) return 0;
+  const xArr = Number.parseInt(String(x).split("").reverse().join(""));
+  return xArr >= -Math.pow(2, 31) && xArr <= Math.pow(2, 31) - 1
+    ? x < 0
+      ? -xArr
+      : xArr
+    : 0;
+};
+console.log(func(x));
